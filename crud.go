1d59b77c4c3f4975783ef1454ad10a85a3ef5720
marianpimiento <ing.mariapimiento@gmail.com>
2018-12-28 20:26:04 -0500

    Funciones insertReceta para actualizar y deleteReceta para eliminar

--- crud.go
+++ crud.go
@@ -3,45 +3,65 @@ package main
 import (
 	"fmt"
 	"log"
-	"reflect"
 )
 
 func main() {
+
+	// CREACION DE UNA NUEVA RECETA
 	//r := Receta{
-	//	nombre: "Pie de Manzana",
+	//	nombre: "Pie de Durazno",
 	//	tipoPlato: "Postre",
 	//	preparacion: "Mezclar todos los ingredientes y hornear",
 	//	porciones: 15,
 	//}
 	//
-	//err := RecetaCrear(r)
+	//_, err := insertReceta(r)
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
 
 
-	//var listaRecetas []Receta
+	//ACTUALIZACION DE UNA RECETA
+	r := Receta{
+		idRecetas: 412471344852271105,
+		nombre: "Mousse de Mora",
+		tipoPlato: "Postre",
+		preparacion: "Mezclar todos los ingredientes y refrigerar por 4 horas",
+		porciones: 10,
+	}
+
+	ID, err := updateReceta(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ID)
+
+
+	//ELIMINACION DE UNA RECETA
+	ID, err = deleteReceta(412467123364823041)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Receta eliminada")
+
+
+	// LISTA LAS RECETAS (CON FILTRO POR NOMBRE)
 	listaRecetas, err := allRecetas("")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("\n+++++ RECETAS +++++\n")
 	for i := 0; i < len(listaRecetas); i++ {
 		fmt.Println(listaRecetas[i])
 	}
 
 
-	//var recetaSeleccionada Receta
-	recetaSeleccionada, err := getReceta(412783567569125377)
-	fmt.Println(reflect.TypeOf(err))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("\n+++++ RECETA SELECCIONADA +++++\n")
-
-	fmt.Println(recetaSeleccionada)
-
+	// LISTA UNA RECETA (POR ID)
+	//recetaSeleccionada, err := getReceta(412783567569125377)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("\n+++++ RECETA SELECCIONADA +++++\n")
+	//fmt.Println(recetaSeleccionada)
 
 }
--- recetas.go
+++ recetas.go
@@ -13,33 +13,13 @@ type Receta struct {
 	porciones int `json:"porciones,omitempty"`
 }
 
-// CRUD
 
-// Crear RECETAS
-func RecetaCrear(r Receta) error {
-	query := `INSERT INTO recetas (nombre, tipoplato, preparacion, porciones)
-	VALUES ($1, $2, $3, $4) RETURNING idrecetas`
-	db := getConnection()
-	defer db.Close()
-
-	stmt, err := db.Prepare(query)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	var idreceta int
-
-	err = db.QueryRow(query, r.nombre, r.tipoPlato , r.preparacion , r.porciones).Scan(&idreceta)
-	if err != nil {
-		return err
-	}
-	fmt.Println("ID receta:", idreceta)
-
-	return nil
-}
-
-
+// Funcion getReceta: Obtiene los datos de una Receta por medio de su id
+// Input:
+// 		- idBusqueda int: Id de la receta a buscar
+// Output:
+// 		- res Receta: Elemento tipo receta con los datos, si no hubo error
+// 		- err error: Error generado, si aplica
 func getReceta(idBusqueda int) (Receta, error) {
 
 	res := Receta{}
@@ -55,15 +35,15 @@ func getReceta(idBusqueda int) (Receta, error) {
 	}
 	defer stmt.Close()
 
-	var id, porciones int
-	var nombre, tipo, preparacion string
+	var idRecetas, porciones int
+	var nombre, tipoPlato, preparacion string
 
-	err = db.QueryRow(query, idBusqueda).Scan(&id, &nombre, &tipo, &preparacion, &porciones)
+	err = db.QueryRow(query, idBusqueda).Scan(&idRecetas, &nombre, &tipoPlato, &preparacion, &porciones)
 	if err == nil {
 		res = Receta{
-			idRecetas: id,
+			idRecetas: idRecetas,
 			nombre: nombre,
-			tipoPlato: tipo,
+			tipoPlato: tipoPlato,
 			preparacion: preparacion,
 			porciones: porciones,
 		}
@@ -73,6 +53,12 @@ func getReceta(idBusqueda int) (Receta, error) {
 }
 
 
+// Funcion allRecetas: Obtiene los datos de las Recetas filtradas con base en el nombre, si el texto de busqueda es vacío regresa todas las recetas
+// Input:
+// 		- txtBusqueda string: Texto para buscar por nombre la receta
+// Output:
+// 		- recetas []Receta: Lista de elementos tipo receta con los datos, si no hubo error
+// 		- err error: Error generado, si aplica
 func allRecetas(txtBusqueda string) ([]Receta, error) {
 
 	recetas := []Receta{}
@@ -95,15 +81,15 @@ func allRecetas(txtBusqueda string) ([]Receta, error) {
 
 	if err == nil {
 		for rows.Next() {
-			var id, porciones int
-			var nombre, tipo, preparacion string
+			var idRecetas, porciones int
+			var nombre, tipoPlato, preparacion string
 
-			err = rows.Scan(&id, &nombre, &tipo, &preparacion, &porciones)
+			err = rows.Scan(&idRecetas, &nombre, &tipoPlato, &preparacion, &porciones)
 			if err == nil {
 				recetaActual := Receta{
-					idRecetas: id,
+					idRecetas: idRecetas,
 					nombre: nombre,
-					tipoPlato: tipo,
+					tipoPlato: tipoPlato,
 					preparacion: preparacion,
 					porciones: porciones,
 				}
@@ -119,4 +105,101 @@ func allRecetas(txtBusqueda string) ([]Receta, error) {
 	}
 
 	return recetas, err
+}
+
+
+// Funcion insertReceta: Crea una nueva receta
+// Input:
+// 		- r Receta: Elemento tipo Receta con los datos para la creacion
+// Output:
+// 		- recetaID int: Id de la nueva receta creada si no hubo error, de lo contrario es cero
+// 		- err error: Error generado, si aplica
+func insertReceta(r Receta) (int, error) {
+
+	var recetaID int
+
+	query := `INSERT INTO recetas (nombre, tipoplato, preparacion, porciones)	VALUES ($1, $2, $3, $4) RETURNING idrecetas`
+	db := getConnection()
+	defer db.Close()
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	err = db.QueryRow(query, r.nombre, r.tipoPlato , r.preparacion , r.porciones).Scan(&recetaID)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println("ID receta:", recetaID)
+
+	return recetaID, nil
+}
+
+
+// Funcion updateReceta: Actualizar una receta
+// Input:
+// 		- r Receta: Elemento tipo Receta con los datos para la actualizacion
+// Output:
+// 		- rowsUpdated int: Cantidad de filas actualizadas si no hubo error, de lo contrario es cero
+// 		- err error: Error generado, si aplica
+func updateReceta(r Receta) (int, error) {
+
+	query := `UPDATE recetas set nombre=$1, tipoplato=$2, preparacion=$3, porciones=$4 where idrecetas=$5 RETURNING idrecetas`
+
+	db := getConnection()
+	defer db.Close()
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	res, err := db.Exec(query, r.nombre, r.tipoPlato , r.preparacion , r.porciones, r.idRecetas)
+	if err != nil {
+		return 0, err
+	}
+
+	rowsUpdated, err := res.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(rowsUpdated), err
+}
+
+
+// Funcion deleteReceta:
+// Input:
+// 		- recetaID int: Id de la receta a eliminar
+// Output:
+// 		- rowsDeleted int:  Cantidad de filas eliminadas si no hubo error, de lo contrario es cero
+// 		- err error: Error generado, si aplica
+func deleteReceta(recetaID int) (int, error) {
+
+	query := `DELETE FROM recetas WHERE idrecetas=$1`
+
+	db := getConnection()
+	defer db.Close()
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	res, err := db.Exec(query, recetaID)
+	if err != nil {
+		return 0, err
+	}
+
+	rowsDeleted, err := res.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(rowsDeleted), nil
+
 }
